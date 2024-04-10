cf6cadc24932ce1bad5d1f0c46ddd025fbdc5d3c
Rohan <rohanbadal075@gmail.com>
2024-04-09 21:00:26 -0400

    implenting exported model (still broken)

--- web/news/views/__init__.py
+++ web/news/views/__init__.py
@@ -1,3 +1,3 @@
 """Views, one for each Insta485 page."""
 #need to update check github to see how i did it.
-from news.views.index import show_index, show_result
+from news.views.index import show_index, show_result, show_evaluation
--- web/news/views/index.py
+++ web/news/views/index.py
@@ -7,11 +7,38 @@ import requests
 import flask
 import re
 import news
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+import h5py
 
 
 @news.app.route("/")
 def show_index():
     return flask.render_template("index.html")
+
+
+
+@news.app.route("/evaluation", methods=["POST"])
+def show_evaluation():
+
+    #json_file = open('news/views/shrink_model.json', 'r')
+    #loaded_model_json = json_file.read()
+    #json_file.close()
+    #loaded_model = tf.keras.models.model_from_json(loaded_model_json)
+    #loaded_model.load_weights("news/views/shrink_model.weights.h5")
+    print(tf.__version__)
+    #model = tf.keras.models.load_model("news/views/shrink_model.keras")
+    #print(model.summary())
+    snippet = flask.request.form["snippet"]
+    # numeric_labels = {'pants-fire':0, 'false':1, 'barely-true':2, 'half-true':3, 'mostly-true':4, 'true':5}
+    #prediction = model.predict(snippet)
+    
+
+    context = {"prediction": "prediction"}
+
+    return flask.render_template("evaluation.html", **context)
+
 
 @news.app.route("/result", methods=["POST"])
 def show_result():
